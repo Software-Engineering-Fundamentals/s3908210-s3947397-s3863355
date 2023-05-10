f5d59006b9da7c5fc22c9332d626a4b654dfc264
Huylimhor <s363355@student.rmit.edu.au>
2023-05-10 16:19:56 +1000

    .

---
+++ financialManager.java
@@ -0,0 +1,13 @@
+
+public class financialManager {
+   
+    public void vadility(){
+        System.out.print("");
+
+    }
+    public boolean processRefund(String registrationID){
+        System.out.print("");
+        return true;
+    }
+}
+
--- main.java
+++ main.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
 public class main {
-    
+
+    public static void main(String[] args) {
+
+        requestForRefund();
+
+    }
+    public static void requestForRefund(){
+        Scanner scan = new Scanner(System.in);
+        String traineeName;
+        double feepaid, refundAmount;
+
+        financialManager refund = new financialManager();
+        refund.vadility();
+        System.out.println("\"The refund can only be processed if you cancel the training 24 hours before the first session\"");
+        System.out.print("Please enter your name: ");
+        traineeName = scan.nextLine();
+
+
+        System.out.println();
+
+
+
+    }
+
+
+
 }
